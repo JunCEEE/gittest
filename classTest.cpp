cca162977209d5ea8445d754bd348587d507e8d0
Juncheng <juncheng.e@hzdr.de>
2019-05-06 22:57:23 +0200

    add overloading

--- classTest.cpp
+++ classTest.cpp
@@ -1,11 +1,13 @@
 // classes example
 #include <iostream>
+
 using namespace std;
 
 class Rectangle {
     int width, height;
   public:
     void set_values (int,int);
+    void set_values (float,float);
     int area() {return width*height;}
 	bool mac;
 };
@@ -15,11 +17,16 @@ void Rectangle::set_values (int x, int y) {
   height = y;
 }
 
+void Rectangle::set_values (float x, float y) {
+  width = x;
+  height = y;
+}
+
 int main () {
   Rectangle rect;
   rect.set_values (3,4);
   rect.mac = 1;
   if (rect.mac)
-  cout << "area: " << rect.area();
+  cout << "area: " << rect.area()<< endl;
   return 0;
 }
